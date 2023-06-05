3273ff7694a06372209195405afa96c6f34113c0
PedroDavid <u19207574@utp.edu.pe>
2023-06-05 01:26:36 -0500

    [ADD] Enemy with Raycast

---
+++ Assets/DarkBullet.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DarkBullet : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    private void Move()
+    {
+        transform.Translate(Vector2.left * Time.deltaTime * 3f);
+    }
+}
